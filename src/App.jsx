eb5b3293d77f047c4e7907d0c33bf872cc289f30
git config --global user.name <saikiran.rfchh@gmail.com>
2025-03-03 15:26:35 +0530

    initial setup
    installation project set up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-lg text-center">
+        <h1 className="text-3xl font-bold text-blue-600">Hello, Welcome!</h1>
+        <p className="text-gray-600 mt-2">Sendo</p>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
